9d083d2f963f90e26530c70701e94ba7ad851a4a
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-12-20 11:36:41 +0200

    iinit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+
+
+export default function App() {
+  return (
+    <View className="flex items-center justify-center flex-col min-h-screen">
+      <View>
+        <Text className="font-semibold mb-4 text-2xl text-gray-900 dark:text-white">
+          Eezi-Badal
+        </Text>
+      </View>
+    </View>
+  );
+}
